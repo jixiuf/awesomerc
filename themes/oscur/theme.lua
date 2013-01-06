74390410a2c1a985572edefd6acc2caba1ffa34f
jixiuf <jixiuf@gmail.com>
2013-01-06 23:30:28 +0800

    [纪秀峰]:path 受影响的文件:     themes/oscur/theme.lua

--- themes/oscur/theme.lua
+++ themes/oscur/theme.lua
@@ -112,7 +112,7 @@ theme.wallpaper_cmd = {  }
 mytimer = timer { timeout = x }
 mytimer:add_signal("timeout", function()
                                    -- tell awsetbg to randomly choose a wallpaper from your wallpaper directory
-                                 os.execute("awsetbg  -F  -r  " .. confdir  .. "/themes/oscur/image&")
+                                 os.execute("awsetbg  -F  -r  " .. confdir  .. "/theme/oscur/image&")
                                  -- stop the timer (we don't need multiple instances running at the same time)
                                  mytimer:stop()
                                    -- define the interval in which the next wallpaper change should occur in seconds
