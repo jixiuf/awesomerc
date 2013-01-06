f1e24f7e4430575e4ecf27b3d4b47b5ae6845de6
jixiuf <jixiuf@gmail.com>
2013-01-06 23:32:17 +0800

    [纪秀峰]:path 受影响的文件:     rc.lua

--- rc.lua
+++ rc.lua
@@ -6,7 +6,7 @@ require("awful.rules")
 require("naughty")
 require("vicious")
 require("beautiful")
-beautiful.init(awful.util.getdir("config") .. "/themes/oscur/theme.lua")
+beautiful.init(awful.util.getdir("config") .. "/theme/oscur/theme.lua")
 -- beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 --动态tags
